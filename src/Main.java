c8eae84bb27452784565e6cdda715f264a5771a4
soymze <soylmze@gmail.com>
2024-01-24 16:45:02 +0300

    ad

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int number,sum=0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Sayıyı giriniz:");
+        number = sc.nextInt();
+
+        if(number == 1){
+            System.out.println("Mükemmel sayı değildir.");
+        }else{
+            for(int i=1; i < number; i++){
+                if(number % i == 0){
+                    sum +=i;
+                }
+            }
+            if(sum == number){
+                System.out.println(number+" Mükemmel sayıdır.");
+            }else{
+                System.out.println(number+" Mükemmel sayı değildir.");
+            }
+        }
     }
 }
